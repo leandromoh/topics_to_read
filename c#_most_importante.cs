41c5758f027230eea4177cba185f353ccf0bf368
Leandro Fernandes <leandromoh@hotmail.com>
2021-06-10 17:07:01 -0300

    Create c#_most_importante.cs

---
+++ c#_most_importante.cs
@@ -0,0 +1,23 @@
+colections array, list, queue, stack, dictionary, hashset
+imutabilidade
+transparency referential
+side-effects
+pure functions
+span, readOnlySpan, stackalloc, arrayPool, IOPipeline
+ref, out, in
+pattern match
+Records
+multiple inheritance vs composition vs decorator pattern
+covariance, invariance
+var vs dynamic vs object
+ IEnumerable / lazy evaluation
+ linq e/ou morelinq 
+ yield return / yield break
+ Async streams
+ box and unbox
+ stack vs heap
+ valueTypes vs referenceTypes
+23 Design Pattern -> Gang of Four -> Gof
+concurrent collections (ConcurrentBag, ConcurrentStack, ConcurrentQueue, etc)
+
+- mongo, docker, kubernets, git, 
